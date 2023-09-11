098dc4a3d51038d10ceb317843cfac5429382eb6
3vinicius <amorimvinicius3@gmail.com>
2023-09-11 17:48:13 -0300

    create compoente and httpClient

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -1,15 +1,51 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ChangeDetectorRef, OnChanges, SimpleChanges} from '@angular/core';
+import { PokemonService } from 'src/app/services/pokemon.service';
 
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
-  styleUrls: ['./card.component.css']
+  styleUrls: ['./card.component.css'],
 })
-export class CardComponent implements OnInit {
+export class CardComponent implements OnInit, OnChanges {
+  value:string = '';
+  name: string = 'galvantula';
+  id: string = '1';
+  type: Array<string> = [];
+  imgSrc: string =
+    'https://www.pngmart.com/files/22/Charmander-Pokemon-PNG-Photos.png';
 
-  constructor() { }
+
+  /* Primieiro declara no construtor */
+  constructor(private services: PokemonService, private cdRef:ChangeDetectorRef) {}
+  ngOnChanges(changes: SimpleChanges): void {
+    throw new Error('Method not implemented.');
+  }
+
 
   ngOnInit(): void {
+    /* Depois chama o service como uma classe e chama o motodo que voce quer */
+    /* Como é retornado um oberver devemos usar o subscribe({}) para se  'escrever'
+    no observer retornado
+      Dentro dele é possivel passar um objeto que conten dois metdoso
+      next -> pra caso seja resolvido
+      error -> caso de erro na requesição
+    */
+    this.search(this.name)
+  }
+
+
+  search(value:string){
+    this.services.getPokemon(value.toLocaleLowerCase()).subscribe({
+      next: (res) => {
+        this.name = res.name.charAt(0).toUpperCase() + res.name.slice(1);
+        this.id = res.id;
+        this.imgSrc = res.sprites.front_default;
+        let myTypes:Array<string>=[]
+        res.types.map((v) => myTypes.push(v.type.name))
+        this.type = myTypes;
+      },
+      error: (err) => window.alert('name not found'),
+    });
   }
 
 }
---
+++ src/app/services/pokemon.service.ts
@@ -0,0 +1,41 @@
+import { Injectable } from '@angular/core';
+import { environment } from '../../environments/environment';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+
+/* É necessario saber o tipo de dado que vai vim para que o tipe script reconheça,
+  Assim é necessário tipar tudo.
+*/
+interface Pokemon {
+  name: string;
+  id: string;
+  sprites: { front_default: string };
+  types: Array<{type:{name:string}}>;
+}
+
+/* Isso é uma construção de injeção de dependencias, assim ele é passado dentro do construtor do component */
+@Injectable({
+  /* Significa que ele está servindo a arvore inteira */
+  providedIn: 'root',
+})
+export class PokemonService {
+  private baseUrl: string = '';
+  private pokeData: any | Pokemon = '';
+
+  /* Injetando dependencias do httpCliente */
+  constructor(private http: HttpClient) {
+    this.baseUrl = environment.pokeApi;
+  }
+
+  /* Observable -> é um conceito de design pattern que criar um observavel
+  Assim possiveis alterações no httpClient é capiturado por ele.
+    Ao usar httpClient ele retornar um Observable e por isso é necessário tipas
+  O metodo e o get com o tipo de dado que será retornado
+  */
+  getPokemon(pokemonName: string): Observable<Pokemon> {
+    /* Fazendo uma requisição get com o httpClient */
+    this.pokeData = this.http.get<Pokemon>(`${this.baseUrl}${pokemonName}`);
+    return this.pokeData;
+  }
+}
